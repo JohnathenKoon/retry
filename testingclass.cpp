02ca82e6a9153c014b942cc433efae12c37f153f
Johnathen Koon <jkoon3@asu.edu>
2019-01-03 15:37:56 -0700

    added test files to commit

---
+++ testingclass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jfkoo on 1/3/2019.
+//
+
+#include "testingclass.h"
---
+++ testingclass.h
@@ -0,0 +1,14 @@
+//
+// Created by jfkoo on 1/3/2019.
+//
+
+#ifndef UNTITLED_TESTINGCLASS_H
+#define UNTITLED_TESTINGCLASS_H
+
+
+class testingclass {
+
+};
+
+
+#endif //UNTITLED_TESTINGCLASS_H
